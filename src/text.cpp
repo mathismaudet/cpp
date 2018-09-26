d373456e544f23d2088408887f3de3968da51cc5
mathis maudet <mathis.maudet@epsi.fr>
2018-09-26 10:30:46 +0200

    library commit

---
+++ src/text.cpp
@@ -0,0 +1,18 @@
+#include "text.h"
+#include <iostream>
+
+void texte::hello()
+{
+	std::cout << "Hello World" << std::endl;
+}
+
+void texte::bonjour()
+{
+	std::cout << "Bonjour" << std::endl;
+}
+
+void texte::cava()
+{
+	std::cout << "Ca va?" << std::endl;
+}
+
---
+++ src/text.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class texte {
+
+	public:
+		void hello();
+		void bonjour();
+		void cava();
+};
